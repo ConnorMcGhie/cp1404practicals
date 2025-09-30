9bd73f6a9fcda1ad8025abc8ddc0a240b061e1fc
ConnorMcGhie <connor.mcghie@my.jcu.edu.au>
2025-09-30 15:13:26 +1000

    Used f-string formatting to produce outputs for TODO tasks

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -32,16 +32,7 @@ numbers = [1, 19, 123, 456, -25]
 for i, number in enumerate(numbers, 1):
     print(f"Number {i} is {number:5}")
 
+print(f"{year} {name} for about ${cost:,.0f}!")
 
-# produce the following right-aligned output (DO NOT use a list):
-# 2 ^ 0 is    1
-# 2 ^ 1 is    2
-# 2 ^ 2 is    4
-# 2 ^ 3 is    8
-# 2 ^ 4 is   16
-# 2 ^ 5 is   32
-# 2 ^ 6 is   64
-# 2 ^ 7 is  128
-# 2 ^ 8 is  256
-# 2 ^ 9 is  512
-# 2 ^10 is 1024
+for power in range(11):
+    print(f"2 ^ {power:2} is {2 ** power:4}")
