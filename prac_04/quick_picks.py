b2764d3c58ba14bae9e01a49950f49c24cd5d659
ConnorMcGhie <connor.mcghie@my.jcu.edu.au>
2025-10-07 13:44:59 +1000

    Add quick_picks.py

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,32 @@
+"""
+CP1404 - Practical
+Quick pick program
+"""
+
+import random
+
+NUMBERS_PER_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
+
+
+def main():
+    """Ask user for number of quick picks and display them."""
+    number_of_quick_picks = int(input("How many quick picks? "))
+    while number_of_quick_picks < 0:
+        print("Number of picks must be a positive integer")
+        number_of_quick_picks = int(input("How many quick picks? "))
+
+    for i in range(number_of_quick_picks):
+        quick_pick = []
+        for j in range(NUMBERS_PER_LINE):
+            number = random.randint(MINIMUM, MAXIMUM)
+            while number in quick_pick:
+                number = random.randint(MINIMUM, MAXIMUM)
+            quick_pick.append(number)
+        quick_pick.sort()
+
+        print(" ".join(f"{number:2}" for number in quick_pick))
+
+
+main()
