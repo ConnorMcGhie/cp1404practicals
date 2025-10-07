4246d4e58a43f0e9d15d704bb2522fdca6d16c6e
ConnorMcGhie <connor.mcghie@my.jcu.edu.au>
2025-10-07 13:29:20 +1000

    Add lower_case_full names comprehension

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -30,7 +30,8 @@ print(a_names)
 print(" ".join(sorted(names)))
 
 # list comprehension to create a list of all the full_names in lowercase format
-# lowercase_full_names =
+lowercase_full_names = [name.lower() for name in full_names]
+print(lowercase_full_names)
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # list comprehension to create a list of integers from the above list of strings
