9f25ba4ec76f959b58d71a852c52d92a4dbc0285
ConnorMcGhie <connor.mcghie@my.jcu.edu.au>
2025-10-30 15:04:21 +1000

    Add programming_language.py and languages.py

---
+++ prac_06/languages.py
@@ -0,0 +1,12 @@
+"""
+CP1404 - Practical
+EST completion time: 30 minutes
+Actual time:
+"""
+
+
+def main():
+    print("PEP8")
+
+
+main()
---
+++ prac_06/programming_language.py
@@ -0,0 +1,12 @@
+"""
+CP1404 - Practical
+EST completion time: 30 minutes
+Actual time:
+"""
+
+
+class ProgrammingLanguage:
+    """Represent information related to a programming language"""
+
+    def __init__(self):
+        """Create programming language from given values"""
