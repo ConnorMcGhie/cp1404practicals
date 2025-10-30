2974c29823458b0f91d115199d474a68c8e99b15
ConnorMcGhie <connor.mcghie@my.jcu.edu.au>
2025-10-30 15:09:15 +1000

    Add __init__ and is_dynamic() methods to ProgrammingLanguage

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -8,5 +8,13 @@ Actual time:
 class ProgrammingLanguage:
     """Represent information related to a programming language"""
 
-    def __init__(self):
+    def __init__(self, name, typing, reflection, year):
         """Create programming language from given values"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Determine if language is dynamically typed."""
+        return self.typing == "Dynamic"
