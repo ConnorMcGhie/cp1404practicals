5c97e05069da5f3fd18d9fad28c7c51321de3e18
ConnorMcGhie <connor.mcghie@my.jcu.edu.au>
2025-10-30 14:44:52 +1000

    Add names to car objects in used_cars.py and print car objects

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -1,4 +1,3 @@
-
 """
 CP1404/CP5632 Practical - Client code to use the Car class.
 Note that the import has a folder (module) in it.
@@ -10,10 +9,16 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("My Car", 180)
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
+
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    print(f"Limo has fuel: {limo.fuel}")
+    limo.drive(115)
+    print(limo)
 
 
 main()
